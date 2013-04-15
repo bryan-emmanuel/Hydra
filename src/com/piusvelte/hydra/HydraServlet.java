f73428ab5361f23b2d33e2303e98414cebacc15b
piusvelte <piusvelte@gmail.com>
2013-04-14 22:19:18 -0400

    add all do methods for api servlet

--- src/com/piusvelte/hydra/HydraServlet.java
+++ src/com/piusvelte/hydra/HydraServlet.java
@@ -22,6 +22,7 @@ package com.piusvelte.hydra;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,96 +30,271 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+@WebServlet(
+		name = "api",
+		urlPatterns = "/api")
 public class HydraServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	static final String ACTION_ABOUT = "about";
+	static final int DATABASE = 0;
+	static final int TARGET = 1;
+
+	static final String PARAM_HMAC = "hmac";
+	static final String PARAM_COLUMNS = "columns";
+	static final String PARAM_SELECTION = "selection";
+	static final String PARAM_VALUES = "values";
+	static final String PARAM_QUEUEABLE = "queueable";
+	static final String PARAM_TARGET = "target";
+	static final String PARAM_ACTION = "action";
+	static final String PARAM_DATABASE = "database";
+
 	static final String ACTION_QUERY = "query";
 	static final String ACTION_INSERT = "insert";
 	static final String ACTION_UPDATE = "update";
 	static final String ACTION_EXECUTE = "execute";
 	static final String ACTION_SUBROUTINE = "subroutine";
 	static final String ACTION_DELETE = "delete";
-	static final String BAD_REQUEST = "bad request";
+	
+	/*
+	 * METHODS:
+	 *  GET - query
+	 *  POST - insert, subroutine, execute
+	 *  PUT - update
+	 *  DELETE - delete
+	 */
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().write("use POST");
-		response.setStatus(401);
+	private boolean isAuthenticated(String hmac, String database, String target, String[] columns, String[] values, String selection, String queueable) {
+		// database + target + params
+		String requestAuth = database + target;
+		if (columns != null) {
+			for (String c : columns)
+				requestAuth += c;
+		}
+		if (values != null) {
+			for (String v : values)
+				requestAuth += v;
+		}
+		requestAuth += selection + queueable;
+		return HydraService.authenticated(null, hmac, requestAuth);
 	}
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HydraRequest hydraRequest = new HydraRequest(request);
-		if ((hydraRequest != null) && HydraService.authenticated(null, hydraRequest.getHMAC(), hydraRequest.getRequestAuth())) {
-			DatabaseConnection databaseConnection = null;
-			// execute, select, update, insert, delete
-			if (ACTION_ABOUT.equals(hydraRequest.action) && (hydraRequest.database.length() == 0)) {
-				response.setStatus(200);
-				response.getWriter().write(HydraService.getDatabases().toJSONString());
-			} else if (hydraRequest.database != null) {
-				if (ACTION_ABOUT.equals(hydraRequest.action)) {
-					response.setStatus(200);
-					response.getWriter().write(HydraService.getDatabase(hydraRequest.database).toJSONString());
-				} else {
+	private String[] getPathParts(String path) {
+		String[] parts = new String[]{null, null};
+		if (path.length() > 0) {
+			if (path.substring(0, 1).equals("/"))
+				path = path.substring(1);
+			if (path.length() > 0) {
+				String[] paths = path.split("/");
+				if (paths[DATABASE].length() > 0)
+					parts[DATABASE] = paths[DATABASE];
+				if ((paths.length > TARGET) && (paths[TARGET].length() > 0))
+					parts[TARGET] = paths[TARGET];
+			}
+		}
+		return parts;
+	}
+	
+	private String getQueuedRequest(String action, String database, String target, String[] columns, String[] values, String selection, boolean queueable) {
+		JSONObject request = new JSONObject();
+		request.put(PARAM_ACTION, action);
+		request.put(PARAM_DATABASE, database);
+		request.put(PARAM_TARGET, target);
+		JSONArray colArr = new JSONArray();
+		for (String s : columns)
+			colArr.add(s);
+		request.put(PARAM_COLUMNS, colArr);
+		JSONArray valArr = new JSONArray();
+		for (String s : values)
+			valArr.add(s);
+		request.put(PARAM_VALUES, valArr);
+		request.put(PARAM_SELECTION, selection);
+		request.put(PARAM_QUEUEABLE, Boolean.toString(queueable));
+		return request.toJSONString();
+	}
+
+	/*
+	 * query
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] parts = getPathParts(request.getRequestURI().substring(request.getContextPath().length()));
+		String database = parts[DATABASE];
+		String target = parts[TARGET];
+		String[] columns = request.getParameterValues(PARAM_COLUMNS);
+		String[] values = request.getParameterValues(PARAM_VALUES);
+		String selection = request.getParameter(PARAM_SELECTION);
+		String q = request.getParameter(PARAM_QUEUEABLE);
+		boolean queueable = false;
+		if (q == null)
+			q = "";
+		else
+			queueable = Boolean.parseBoolean(q);
+		if (isAuthenticated(request.getParameter(PARAM_HMAC), database, target, columns, values, selection, q)) {
+			if (database != null) {
+				if (target != null) {
+					DatabaseConnection databaseConnection = null;
 					try {
-						databaseConnection = HydraService.getDatabaseConnection(hydraRequest.database);
+						databaseConnection = HydraService.getDatabaseConnection(database);
+						response.getWriter().write(databaseConnection.query(target, columns, selection).toJSONString());
 					} catch (Exception e) {
 						e.printStackTrace();
+						if (queueable) {
+							JSONObject j = new JSONObject();
+							JSONArray errors = new JSONArray();
+							errors.add("no database connection");
+							HydraService.queueRequest(getQueuedRequest(ACTION_QUERY, database, target, columns, values, selection, queueable));
+							errors.add("queued");
+							response.setStatus(200);
+							j.put("errors", errors);
+							response.getWriter().write(j.toJSONString());
+						} else
+							response.setStatus(502);
 					}
-					if (databaseConnection == null) {
-						// queue
+				} else
+					response.getWriter().write(HydraService.getDatabase(database).toJSONString());
+			} else
+				response.getWriter().write(HydraService.getDatabases().toJSONString());
+		} else {
+			response.setStatus(401);
+		}
+	}
+
+	/*
+	 * insert
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] parts = getPathParts(request.getRequestURI().substring(request.getContextPath().length()));
+		String database = parts[DATABASE];
+		String target = parts[TARGET];
+		String[] columns = request.getParameterValues(PARAM_COLUMNS);
+		String[] values = request.getParameterValues(PARAM_VALUES);
+		String selection = request.getParameter(PARAM_SELECTION);
+		String q = request.getParameter(PARAM_QUEUEABLE);
+		boolean queueable = false;
+		if (q == null)
+			q = "";
+		else
+			queueable = Boolean.parseBoolean(q);
+		if (isAuthenticated(request.getParameter(PARAM_HMAC), database, target, columns, values, selection, q)) {
+			if ((database != null) && (target != null)) {
+				DatabaseConnection databaseConnection = null;
+				String action = null;
+				try {
+					databaseConnection = HydraService.getDatabaseConnection(database);
+					if (columns.length > 0) {
+						action = ACTION_INSERT;
+						response.getWriter().write(databaseConnection.insert(target, columns, values).toJSONString());
+					} else if (values.length > 0) {
+						action = ACTION_SUBROUTINE;
+						response.getWriter().write(databaseConnection.subroutine(target, values).toJSONString());
+					} else {
+						action = ACTION_EXECUTE;
+						response.getWriter().write(databaseConnection.execute(target).toJSONString());
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					if (queueable) {
 						JSONObject j = new JSONObject();
 						JSONArray errors = new JSONArray();
 						errors.add("no database connection");
-						if (hydraRequest.queueable) {
-							HydraService.queueRequest(hydraRequest.getRequest());
-							errors.add("queued");
-							response.setStatus(200);
-						} else
-							response.setStatus(400);
+						HydraService.queueRequest(getQueuedRequest(action, database, target, columns, values, selection, queueable));
+						errors.add("queued");
+						response.setStatus(200);
 						j.put("errors", errors);
 						response.getWriter().write(j.toJSONString());
-					} else {
-						response.setStatus(200);
-						if (ACTION_EXECUTE.equals(hydraRequest.action) && (hydraRequest.target.length() > 0))
-							response.getWriter().write(databaseConnection.execute(hydraRequest.target).toJSONString());
-						else if (ACTION_QUERY.equals(hydraRequest.action) && (hydraRequest.target.length() > 0) && (hydraRequest.columns.length > 0))
-							response.getWriter().write(databaseConnection.query(hydraRequest.target, hydraRequest.columns, hydraRequest.selection).toJSONString());
-						else if (ACTION_UPDATE.equals(hydraRequest.action) && (hydraRequest.target.length() > 0) && (hydraRequest.columns.length > 0) && (hydraRequest.values.length > 0))
-							response.getWriter().write(databaseConnection.update(hydraRequest.target, hydraRequest.columns, hydraRequest.values, hydraRequest.selection).toJSONString());
-						else if (ACTION_INSERT.equals(hydraRequest.action) && (hydraRequest.target.length() > 0) && (hydraRequest.columns.length > 0) && (hydraRequest.values.length > 0))
-							response.getWriter().write(databaseConnection.insert(hydraRequest.target, hydraRequest.columns, hydraRequest.values).toJSONString());
-						else if (ACTION_DELETE.equals(hydraRequest.action) && (hydraRequest.target.length() > 0))
-							response.getWriter().write(databaseConnection.delete(hydraRequest.target, hydraRequest.selection).toJSONString());
-						else if (ACTION_SUBROUTINE.equals(hydraRequest.action) && (hydraRequest.target.length() > 0) && (hydraRequest.values.length > 0))
-							response.getWriter().write(databaseConnection.subroutine(hydraRequest.target, hydraRequest.values).toJSONString());
-						else {
-							response.setStatus(400);
-							JSONObject j = new JSONObject();
-							JSONArray errors = new JSONArray();
-							errors.add(BAD_REQUEST);
-							j.put("errors", errors);
-							response.getWriter().write(j.toJSONString());
-						}
-						// release the connection
-						databaseConnection.release();
-					}
+					} else
+						response.setStatus(502);
 				}
-			} else {
-				response.setStatus(400);
-				JSONObject j = new JSONObject();
-				JSONArray errors = new JSONArray();
-				errors.add(BAD_REQUEST);
-				j.put("errors", errors);
-				response.getWriter().write(j.toJSONString());
-			}
+			} else
+				response.setStatus(402);
 		} else {
-			response.setStatus(400);
-			JSONObject j = new JSONObject();
-			JSONArray errors = new JSONArray();
-			errors.add(BAD_REQUEST);
-			j.put("errors", errors);
-			response.getWriter().write(j.toJSONString());
+			response.setStatus(401);
+		}
+	}
+
+	/*
+	 * update
+	 */
+	public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] parts = getPathParts(request.getRequestURI().substring(request.getContextPath().length()));
+		String database = parts[DATABASE];
+		String target = parts[TARGET];
+		String[] columns = request.getParameterValues(PARAM_COLUMNS);
+		String[] values = request.getParameterValues(PARAM_VALUES);
+		String selection = request.getParameter(PARAM_SELECTION);
+		String q = request.getParameter(PARAM_QUEUEABLE);
+		boolean queueable = false;
+		if (q == null)
+			q = "";
+		else
+			queueable = Boolean.parseBoolean(q);
+		if (isAuthenticated(request.getParameter(PARAM_HMAC), database, target, columns, values, selection, q)) {
+			if ((database != null) && (target != null)) {
+				DatabaseConnection databaseConnection = null;
+				try {
+					databaseConnection = HydraService.getDatabaseConnection(database);
+					response.getWriter().write(databaseConnection.update(target, columns, values, selection).toJSONString());
+				} catch (Exception e) {
+					e.printStackTrace();
+					if (queueable) {
+						JSONObject j = new JSONObject();
+						JSONArray errors = new JSONArray();
+						errors.add("no database connection");
+						HydraService.queueRequest(getQueuedRequest(ACTION_UPDATE, database, target, columns, values, selection, queueable));
+						errors.add("queued");
+						response.setStatus(200);
+						j.put("errors", errors);
+						response.getWriter().write(j.toJSONString());
+					} else
+						response.setStatus(502);
+				}
+			} else
+				response.setStatus(402);
+		} else {
+			response.setStatus(401);
+		}
+	}
+
+	/*
+	 * delete
+	 */
+	public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] parts = getPathParts(request.getRequestURI().substring(request.getContextPath().length()));
+		String database = parts[DATABASE];
+		String target = parts[TARGET];
+		String[] columns = request.getParameterValues(PARAM_COLUMNS);
+		String[] values = request.getParameterValues(PARAM_VALUES);
+		String selection = request.getParameter(PARAM_SELECTION);
+		String q = request.getParameter(PARAM_QUEUEABLE);
+		boolean queueable = false;
+		if (q == null)
+			q = "";
+		else
+			queueable = Boolean.parseBoolean(q);
+		if (isAuthenticated(request.getParameter(PARAM_HMAC), database, target, columns, values, selection, q)) {
+			if ((database != null) && (target != null)) {
+				DatabaseConnection databaseConnection = null;
+				try {
+					databaseConnection = HydraService.getDatabaseConnection(database);
+					response.getWriter().write(databaseConnection.delete(target, selection).toJSONString());
+				} catch (Exception e) {
+					e.printStackTrace();
+					if (queueable) {
+						JSONObject j = new JSONObject();
+						JSONArray errors = new JSONArray();
+						errors.add("no database connection");
+						HydraService.queueRequest(getQueuedRequest(ACTION_DELETE, database, target, columns, values, selection, queueable));
+						errors.add("queued");
+						response.setStatus(200);
+						j.put("errors", errors);
+						response.getWriter().write(j.toJSONString());
+					} else
+						response.setStatus(502);
+				}
+			} else
+				response.setStatus(402);
+		} else {
+			response.setStatus(401);
 		}
 	}
 
